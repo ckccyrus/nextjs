abf4c611dc925701baa59634013e1438c461c9d3
ckccyrus <>
2024-08-01 16:21:47 -0400

    dev: update session bg color; add github secret and id

--- nextjs-next-auth-js/auth.ts
+++ nextjs-next-auth-js/auth.ts
@@ -1,27 +1,12 @@
 import NextAuth, { NextAuthConfig } from "next-auth"
 import type { Provider } from "next-auth/providers"
 
-import Credentials from "next-auth/providers/credentials"
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
 
 const providers: Provider[] = [
-    // Credentials({
-    //     // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-    //     // e.g. domain, username, password, 2FA token, etc.
-    //     credentials: {
-    //         email: {},
-    //         password: {},
-    //     },
-    //     authorize: async (credentials) => {
-    //         let user = null;
-
-
-    //         return user
-    //     }
-    // }),
     GitHub,
-    Google
+    // Google
 ];
 
 const config = {
@@ -29,16 +14,13 @@ const config = {
     callbacks: {
         jwt({ token, trigger, session, account }) {
             if (trigger === "update") token.name = session.user.name
-            // if (account?.provider === "keycloak") {
-            //     return { ...token, accessToken: account.access_token }
-            // }
             return token
         },
     },
-    pages: {
-        signIn: "/auth/signin"
-    },
-    debug: process.env.NODE_ENV !== "production" ? true : false,
+    // pages: {
+    //     signIn: "/auth/signin"
+    // },
+    // debug: process.env.NODE_ENV !== "production" ? true : false,
 } satisfies NextAuthConfig
 
 export const providerMap = providers.map((provider) => {
--- nextjs-next-auth-js/components/session-data.tsx
+++ nextjs-next-auth-js/components/session-data.tsx
@@ -3,9 +3,9 @@ import type { Session } from "next-auth";
 export default function SessionData({ session }: { session: Session | null }) {
     if (session?.user) {
         return (
-            <div className="flex flex-col gap-4 p-4 w-full bg-gray-100 rounded-md">
-                <div className="flex flex-col rounded-md bg-neutral-100">
-                    <div className="text-xl p-4 font-bold rounded-t-md bg-neutral-200">
+            <div className="flex flex-col gap-4 p-4 w-full rounded-md">
+                <div className="flex flex-col rounded-md bg-neutral-700">
+                    <div className="text-xl p-4 font-bold rounded-t-md bg-neutral-600">
                         Current Session Data
                     </div>
                     <pre className="py-6 px-4 whitespace-pre-wrap break-all">
@@ -17,7 +17,7 @@ export default function SessionData({ session }: { session: Session | null }) {
     }
 
     return (
-        <p className="text-xl p-4 font-bold w-full bg-neutral-200 rounded-md">
+        <p className="text-xl p-4 font-bold w-full bg-neutral-600 rounded-md">
             No session data, please <em>Sign In</em> first.
         </p>
     )
